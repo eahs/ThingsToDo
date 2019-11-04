0e866b3a66ce9ab73bb37f2b508b1be94c1bfbf4
Daniel <dcichocki27@hotmail.com>
2019-11-04 11:15:37 -0500

    Successful MapPage

--- Source/DoingThing/DoingThing/Views/MapPage.xaml.cs
+++ Source/DoingThing/DoingThing/Views/MapPage.xaml.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using DoingThing.Models;
+using DoingThing.Views;
+using DoingThing.ViewModels;
+
 namespace DoingThing.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MapPage : ContentPage
     {
+
         public MapPage()
         {
             InitializeComponent();
+
         }
-
-
 
     }
 }
