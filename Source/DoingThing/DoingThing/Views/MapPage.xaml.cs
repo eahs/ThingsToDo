2d90415536c8dc7cb222eee1ae5734cdbe65bc3c
Daniel <dcichocki27@hotmail.com>
2019-11-07 11:15:09 -0500

    Map Image

--- Source/DoingThing/DoingThing/Views/MapPage.xaml.cs
+++ Source/DoingThing/DoingThing/Views/MapPage.xaml.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.ComponentModel;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-using DoingThing.Models;
-using DoingThing.Views;
-using DoingThing.ViewModels;
+using Xamarin.Essentials;
 
 namespace DoingThing.Views
 {
