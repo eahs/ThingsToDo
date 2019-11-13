b19486e14282043bf774e3bd1e3ad22d5e2f8ca5
Daniel <dcichocki27@hotmail.com>
2019-11-13 10:32:58 -0500

    Google Maps Implemented

--- Source/DoingThing/DoingThing/Views/MapPage.xaml.cs
+++ Source/DoingThing/DoingThing/Views/MapPage.xaml.cs
@@ -1,10 +1,18 @@
 ﻿using System.ComponentModel;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 using Xamarin.Essentials;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
 
 namespace DoingThing.Views
 {
     [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
 
@@ -14,5 +22,26 @@ namespace DoingThing.Views
 
         }
 
+        private async void ButtonOpenCoords_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(EntryLatitude.Text, out double lat))
+                return;
+
+            if (!double.TryParse(EntryLongitude.Text, out double lng))
+                return;
+
+
+
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions
+            {
+                Name = EntryName.Text,
+                NavigationMode = NavigationMode.Default
+            });
+
+        }
+
+
     }
+
 }
+   
