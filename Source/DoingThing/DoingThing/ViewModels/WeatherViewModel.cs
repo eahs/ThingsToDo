ac00756e1788679e4e00a536bf0bc50c8ad3f90c
Daniel <dcichocki27@hotmail.com>
2019-11-15 10:32:08 -0500

    I

---
+++ Source/DoingThing/DoingThing/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using DoingThing.Models;
+using DoingThing.Views;
+
+namespace DoingThing.ViewModels
+{
+    public class WeatherViewModel : BaseViewModel
+    {
+
+        public WeatherViewModel()
+        {
+            Title = "Weather";
+        }
+
+    }
+}
---
+++ Source/DoingThing/DoingThing/Views/WeatherPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace DoingThing.Views
+{
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
+
