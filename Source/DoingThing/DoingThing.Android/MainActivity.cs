e35f4d7ee430ebaf846e21e4c81cf4ea76f134d8
Daniel <dcichocki27@hotmail.com>
2020-02-27 11:33:06 -0500

    Android Works

--- Source/DoingThing/DoingThing.Android/MainActivity.cs
+++ Source/DoingThing/DoingThing.Android/MainActivity.cs
@@ -17,9 +17,7 @@ namespace DoingThing.Droid
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+        { 
 
             base.OnCreate(savedInstanceState);
 
